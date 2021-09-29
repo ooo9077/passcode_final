aebf044178520970fd72d9044e0a38390f6e2b10
Tan-Qian <648936463@qq.com>
2021-09-29 08:13:50 +0800

    changev8

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,43 @@
 <template>
-  <div>
-    <div style="text-align: left;"> 
-      <span style="position:relative;display: inline-block; left:5%; width:65%;">
-        <p style="color:white;font-size:150%">姓名：{{full_name}}</p>
-        <p style="color:white;font-size:150%">部门：{{org_name}}</p>
+  <div style="background: #0984BD;">
+    <div style="text-align: left; position:relative;width:100%;bottom:2%;"> 
+      <span style="position:relative;display: inline-block; left:5%; width:70%;">
+        <div style="color:white;font-size:120%;position:relative;top:0px;">姓名：{{full_name}}</div>
+        <br>
+        <div style="color:white;font-size:120%;position:relative;top:0px;">部门：{{org_name}}</div>
+        <br>
       </span> 
-      <img src="../assets/fuda-logo-blue.png" style="width:30%;height:30%;padding:0px;position:relative;top:20px;"/>
+      <img src="../assets/fuda-logo-blue.png" style="width:25%;height:25%;position:relative;bottom:4px;"/>
     </div>
     
     <div class="qrcanvas">
         <br>
-        <img id="barcode" style="width:95%"/>
+        <img id="barcode" style="width:95%;position:relative;left:3%;"/>
+
+        <div style="text-align:center;">
+        <div style="color:#CC00FF;position:relative;display: inline-block;font-weight:bold">{{currentdate}}</div>
+        <div style="color:#CC00FF;font-size:300%;position:relative;display: inline-block;font-weight:bold">{{currentsec}}</div>
+        <div style="color:#CC00FF;position:relative;display: inline-block;font-weight:bold">{{currenthour}}</div>
+        </div>
         <br>
-        <h3 style="color:green;">{{currentdate}}</h3>
-        <h3 style="color:green;font-size:300%;">{{currentsec}}</h3>
-        <h3 style="color:green;">{{currenthour}}</h3><br>
-        <div style="display: inline-block;margin: 0 10px;text-align: center;"  id="qrcode" ref="qrcode"></div>
-        <p style="text-align: center; color:white;">{{currentTime}}</p>
-        <marquee><span style="font-weight: bolder;font-size: 40px;color: green;">{{large_word}}</span></marquee>
-        <p style="position:relative;width:95%;color:green;text-align:left;left:5%;font-size:100%;">未见异常，允许通行，请主动出示，配合检测，并做好自我防护，出行前请确认。</p>
-        <div style="position: relative; left:-25%">
-            <img src="../assets/yikatong-logo.jpg" style="width:5%;height:5%;padding:0px;"/>
-            <p style="display: inline-block;text-align:left">一卡通<br>适用于所有场景</p>
+        <div style="display: inline-block;text-align: center;position:relative;left:27%" id="qrcode" ref="qrcode"></div>
+        
+        <marquee><div style="font-size: 300%;color: green;position:relative;">{{large_word}}</div></marquee>
+        <div style="position:relative;width:85%;color:green;text-align:left;left:10%;font-size:80%;position:relative;">未见异常，允许通行，请主动出示，配合检测，并做好自我防护，出行前请确认。</div>
+        <br>
+        <div style="position: relative; left:5%">
+            <img src="../assets/yikatong-logo.jpg" style="position:relative;left:5%;bottom:8px;width:6%;height:6%;"/>
+            <div style="display: inline-block;text-align:left;position: relative; left:5%">一卡通<br>适用于所有场景</div>
+            <br><br>
         </div>
     </div>
     <br>
-    <div style="position: relative; left:25%;border: 1px solid #FFF; border-radius: 5px; width:40%;color:white;font-size:120%" v-on:click="inputBoxHideShow()">每日健康填报</div>
+    <div style="position: relative;font-weight:bold; left:30%;border: 1px solid #FFF; border-radius: 5px; width:40%;color:white;font-size:120%;text-align:center;" v-on:click="inputBoxHideShow()">每日健康填报</div>
     <br>
     <input v-model="full_name" placeholder="姓名" v-if="full_name_show">
     <input v-model="org_name" placeholder="部门" v-if="org_name_show">
     <input v-model="large_word" placeholder="滚动文字" v-if="large_word_show"><!--更新滚动文字-->
+    <div style="text-align: center; color:#0984BD;position:relative;">{{currentTime}}</div>
   </div>
 </template>
 
@@ -49,10 +57,13 @@ export default {
       currentTime: new Date(), // 当前时间 
       downloadButton:false,
       configs:{
-          value:"helloworld!",
+          value:"",
           imagepath:"passqrcode/passqrcode/static/123455.png",
           filter:"color"
       },
+      full_name:'',
+      org_name:'',
+      large_word:'',
       full_name_show:false,
       org_name_show:false,
       large_word_show:false,
@@ -77,6 +88,19 @@ export default {
             
         },
     methods: {
+            RandomRangeNumber(max,min){
+                return Math.round(Math.random()*(max-min+1)+min);
+            },
+            RandomString(len){
+                len = len ||32;
+                var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+                var maxPos = $chars.length;
+                var pwd = '';
+                for(var i = 0; i< len ; i++){
+                    pwd += $chars.charAt(Math.floor(Math.random()*maxPos));
+                }
+                return pwd;
+            },
             //时间过滤加0
             appendZero(obj) {
                 if (obj < 10) {
@@ -86,12 +110,16 @@ export default {
                 }
             },
             qrcode(){
+                var qrtext = this.RandomRangeNumber(60000,10000)+'';
                 let qrcode = new QRCode("qrcode",{
-                    text:"helloworld!"
+                    width:150,
+                    height:150,
+                    text:qrtext
                 });
             },
             barcode(){
-                let barcode=new JsBarcode('#barcode', 'HelloWorld',{
+                var bartext = this.RandomString(7);
+                let barcode=new JsBarcode('#barcode', bartext,{
                     format:'CODE39',
                     lineColor:'#000',
                     width:3,
@@ -119,17 +147,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 h3{
     display: inline-block;
 }
@@ -145,6 +162,7 @@ a {
     position: relative;
     left: 5%;
     width: 90%;
+    height: 80%;
     border-top-right-radius:10px;
     border-top-left-radius:10px;
     border-bottom-left-radius:10px;
